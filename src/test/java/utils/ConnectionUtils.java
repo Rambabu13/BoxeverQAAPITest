ce988478e90594c1232727d84d377a441189a5b9
Luthluniel <belen.martin@gmail.com>
2020-12-30 16:55:28 +0100

    Aclarations

--- src/test/java/utils/ConnectionUtils.java
+++ src/test/java/utils/ConnectionUtils.java
@@ -11,6 +11,14 @@ public class ConnectionUtils {
     private static int lastCode = 0;
     private static String lastErrorMessage = "";
 
+    /**
+     * Function to get a GIST from GITHUB and cast it to a String
+     * @param authToken User token to connect to GITHUB
+     * @param id ID of the GIST we want to retrieve
+     * @param operation If needed, the operation to perform
+     * @return The desired GIST as a String
+     * @throws IOException
+     */
     public static String get (final String authToken, final String id, final String operation)
             throws IOException {
         final URL target = new URL("https://api.github.com/gists/"+ id + operation);
@@ -28,6 +36,12 @@ public class ConnectionUtils {
         return res;
     }
 
+    /**
+     * Function to connect to GITHUB to obtain a GIST
+     * @param stream Connection data string
+     * @return A Gist from HITBUG as a String
+     * @throws IOException
+     */
     public static String getResponse(final InputStream stream)
             throws IOException {
         StringBuilder full = new StringBuilder();
@@ -47,6 +61,14 @@ public class ConnectionUtils {
         return full.toString();
     }
 
+    /**
+     * Function to create a GIST
+     * @param authToken User token to connect to GITHUB
+     * @param operation If needed, the operation to perform
+     * @param gist The basic data to create a GIST in GITHUB
+     * @return The connection object to be used in the main test
+     * @throws IOException
+     */
     public static HttpsURLConnection post(final String authToken, final String operation, final String gist)
             throws IOException {
         final URL target = new URL("https://api.github.com/gists" + operation);
@@ -70,6 +92,13 @@ public class ConnectionUtils {
 
     }
 
+    /**
+     * Function to delete a GIST from GITHUB
+     * @param authToken User token to connect to GITHUB
+     * @param operation If needed, the operation to perform
+     * @return The connection object to be used in the main test
+     * @throws IOException
+     */
     public static HttpsURLConnection delete(final String authToken, final String operation)
             throws IOException {
         final URL target = new URL("https://api.github.com/gists" + operation);
@@ -82,11 +111,20 @@ public class ConnectionUtils {
         return connection;
     }
 
+    /**
+     * Function to code the user token to a basic authentication to connect to GITHUB
+     * @param token User token to connect to GITHUB
+     * @return The connection String
+     */
     static String setBasicAuth(String token) {
         String completeToken = token + ":x-oauth-basic";
         return "Basic " + Base64.getEncoder().encodeToString(completeToken.getBytes());
     }
 
+    /**
+     * Function to keep the last connection data used
+     * @param conn The connection objet used in the test
+     */
     public static void assignLast(HttpsURLConnection conn) {
         try {
             lastCode = conn.getResponseCode();
